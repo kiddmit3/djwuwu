ad5cf16e74b88c7907fc39dca3b963d22cfb072d
David Lac <kiddmit3@gmail.com>
2018-06-28 16:21:03 -0700

    venues section

--- react-ui/src/Components/Music.js
+++ react-ui/src/Components/Music.js
@@ -48,6 +48,24 @@ import React, { Component } from "react";
 
 
 // <MusicPlayer playlist={playlist} />
+
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const images = importAll(require.context('../img/venues/', false, /\.(png|jpe?g|svg)$/));
+
+let venues = [];
+for (var key in images) {
+  var obj = {src: images[key],
+    alt: key.slice(0, -4)};
+  venues.push(obj);
+}
+
+console.log(venues);
+
 class Music extends Component {
   render() {
     return (
@@ -55,8 +73,29 @@ class Music extends Component {
       <div className="card p-4">
         <div className="float-right mb-3"><i className="fas fa-music fa-lg float-right"></i></div>
         <iframe width="100%" height="450" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/316497497&color=%23ba489a&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>        </div>
+        
         <div className="card p-4 mt-4">
         <h4>Venues</h4>
+
+      <div className="card-columns">
+
+{venues.map(function(imageProps) {
+  return (
+    <div className="mb-3 d-inline-block" key={ imageProps.src }>
+      <img className="img-fluid grow-xs" src={ imageProps.src } alt={ imageProps.alt } />
+      { imageProps.alt }
+    </div>
+  );
+})}
+
+
+
+
+      </div>
+
+
+
+
         </div>
 
       </div>
